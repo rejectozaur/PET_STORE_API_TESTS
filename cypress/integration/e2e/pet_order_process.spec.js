7817241847d71a365056b2a8c6d7c527c4f56a06
rejectozaur <14315126+rejectozaur@users.noreply.github.com>
2020-08-21 15:00:34 +0200

    fix spaces

--- cypress/integration/e2e/pet_order_process.spec.js
+++ cypress/integration/e2e/pet_order_process.spec.js
@@ -1,79 +1,79 @@
 var pet = null
 
 describe('Order management process:', () => {
-    it('looks for and oders some available pet ', () => {
-        cy
-            .request('GET', '/pet/findByStatus?status=available') // testowanie base_url
-            .then((response) => {
-                expect(response.status).to.eq(200)
-                pet = response.body[0]
-            }) 
-    })   
+  it('looks for and oders some available pet ', () => {
+    cy
+      .request('GET', '/pet/findByStatus?status=available') // testowanie base_url
+      .then((response) => {
+          expect(response.status).to.eq(200)
+          pet = response.body[0]
+      }) 
+  })   
 
-    it('places an order for the pet ', () => {
-        cy
-            .request('POST', 'store/order', {
-                'petId': pet.id,
-                'quantity': 1,
-                'shipDate': '2020-08-18T05:28:31.610Z',
-                'status': 'placed',
-                'complete': true
-            })
-            .then((response) => {
-                expect(response.status).to.eq(200)
-                expect(response.body).to.deep.include({status: 'placed', complete: true})
-            })
-    })
+  it('places an order for the pet ', () => {
+    cy
+      .request('POST', 'store/order', {
+          'petId': pet.id,
+          'quantity': 1,
+          'shipDate': '2020-08-18T05:28:31.610Z',
+          'status': 'placed',
+          'complete': true
+      })
+      .then((response) => {
+          expect(response.status).to.eq(200)
+          expect(response.body).to.deep.include({status: 'placed', complete: true})
+      })
+  })
 
-    it('odered pet\'s status gets updated to "pending order"', () => {
-        cy
-            .request({
-                method: 'POST', 
-                url: 'pet/' + pet.id, // using update with form data endpoint
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'accept': 'application/json'
-                },
-                body: {
-                'id': pet.id,
-                'name': pet.name,
-                'status': 'pending'
-                }
-            })
-            .then((response) => {      
-                expect(response.status).to.eq(200)
-            })
-        cy
-            .request({method: 'GET', url: 'pet/' + pet.id}).then((response) => {
-                expect(response.body).to.deep.include({name: pet.name, status: 'pending'})
-            })
-    })
+  it('odered pet\'s status gets updated to "pending order"', () => {
+    cy
+      .request({
+          method: 'POST', 
+          url: 'pet/' + pet.id, // using update with form data endpoint
+          headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'accept': 'application/json'
+          },
+          body: {
+          'id': pet.id,
+          'name': pet.name,
+          'status': 'pending'
+          }
+      })
+      .then((response) => {      
+          expect(response.status).to.eq(200)
+      })
+    cy
+      .request({method: 'GET', url: 'pet/' + pet.id}).then((response) => {
+          expect(response.body).to.deep.include({name: pet.name, status: 'pending'})
+      })
+  })
 
-    it('purchased pet\'s status gets updated to "purchased"', () => {
-        cy
-            .request('PUT', 'pet', { // using plain update endpoint
-                'id': pet.id,
-                'name': pet.name,
-                'status': 'purchased'
-            })
-            .then((response) => {
-                expect(response.status).to.eq(200)         
-                expect(response.body).to.deep.include({name: pet.name, status: 'purchased'})
-            })
-    })
+  it('purchased pet\'s status gets updated to "purchased"', () => {
+    cy
+      .request('PUT', 'pet', { // using plain update endpoint
+          'id': pet.id,
+          'name': pet.name,
+          'status': 'purchased'
+      })
+      .then((response) => {
+          expect(response.status).to.eq(200)         
+          expect(response.body).to.deep.include({name: pet.name, status: 'purchased'})
+      })
+  })
 
-    it('deletes this pet from the store', () => {
-        cy
-            .request('DELETE', 'pet/' + pet.id).then((response) => {
-                expect(response.status).to.eq(200)
-            })
-    })
+  it('deletes this pet from the store', () => {
+    cy
+      .request('DELETE', 'pet/' + pet.id).then((response) => {
+          expect(response.status).to.eq(200)
+      })
+  })
 
-    it('verifies this pet is no longer displayed to users', () => {
-        cy
-            .request({method: 'GET', url: 'pet/' + pet.id, failOnStatusCode: false}).then((response) => {
-                expect(response.status).to.eq(404)
-                expect(response.body).to.deep.include({message: 'Pet not found'})
-            })
-    })
+  it('verifies this pet is no longer displayed to users', () => {
+    cy
+      .request({method: 'GET', url: 'pet/' + pet.id, failOnStatusCode: false}).then((response) => {
+          expect(response.status).to.eq(404)
+          expect(response.body).to.deep.include({message: 'Pet not found'})
+      })
+  })
 })
--- cypress/integration/e2e/user_account_management.js
+++ cypress/integration/e2e/user_account_management.js
@@ -1,44 +1,44 @@
 var uniqueNr = Date.now().toString()
 
 describe('Basic user\'s operations:', () => {
-    it('creates an account', () => {
-        cy
-            .request('POST', 'user', {
-                'username': 'automated_tester_' + uniqueNr,
-                'firstName': 'John',
-                'lastName': 'Doe',
-                'email': 'test' + uniqueNr + '@email.com',
-                'password': 'Password123!',
-                'phone': '123 123 123',
-                'userStatus': 0
-            })
-            .then((response) => { // response.body is automatically serialized into JSON
-                expect(response.status).to.eq(200)
-        })
+  it('creates an account', () => {
+    cy
+      .request('POST', 'user', {
+        'username': 'automated_tester_' + uniqueNr,
+        'firstName': 'John',
+        'lastName': 'Doe',
+        'email': 'test' + uniqueNr + '@email.com',
+        'password': 'Password123!',
+        'phone': '123 123 123',
+        'userStatus': 0
+      })
+      .then((response) => { // response.body is automatically serialized into JSON
+        expect(response.status).to.eq(200)
     })
+  })
 
-    it('logs user in into the system', () => {
-        cy
-            .request('GET', 'user/login', {
-                'username': 'automated tester',
-                'password': 'Password123!',
-            })
-            .then((response) => {
-                expect(response.status).to.eq(200)   
-                let regex = new RegExp(/[0-9]+/g)
-                let returned_message = response.body.message
-                let session_id = returned_message.match(regex)  
-                expect(returned_message).to.deep.eql('logged in user session:' + session_id)
-        })
+  it('logs user in into the system', () => {
+    cy
+      .request('GET', 'user/login', {
+        'username': 'automated tester',
+        'password': 'Password123!',
+      })
+      .then((response) => {
+        expect(response.status).to.eq(200)   
+        let regex = new RegExp(/[0-9]+/g)
+        let returned_message = response.body.message
+        let session_id = returned_message.match(regex)  
+        expect(returned_message).to.deep.eql('logged in user session:' + session_id)
     })
+  })
 
-    it('logs user out', () => {
-        cy
-            .request('GET', '/' + 'user/logout', {
-            })
-            .then((response) => {
-                expect(response.status).to.eq(200)
-                expect(response.body).to.deep.include({message: 'ok'})
-        })
+  it('logs user out', () => {
+    cy
+      .request('GET', '/' + 'user/logout', {
+      })
+      .then((response) => {
+        expect(response.status).to.eq(200)
+        expect(response.body).to.deep.include({message: 'ok'})
     })
+  })
 })
